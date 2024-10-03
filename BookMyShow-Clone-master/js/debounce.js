f3b582252d7a6504e0b2e0c7d574a361c0828785
Shubham Dhiman <shubhamdhiman7011@gmail.com>
2024-10-03 13:51:51 +0530

    moviedetails

--- BookMyShow-Clone-master/js/debounce.js
+++
@@ -1,9 +0,0 @@
-// Debounce utility to limit API calls during typing
-function debounce(func, delay) {
-    let timeout;
-    return (...args) => {
-      clearTimeout(timeout);
-      timeout = setTimeout(() => func(...args), delay);
-    };
-  }
-  
